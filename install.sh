65eadbfec5d1752c10b08f164392551bc585fa0a
Neal Grantham <neal@nsgrantham.com>
2019-06-11 15:14:12 -0700

    Add VS Code dotfiles

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #! /usr/bin/env bash
 
-# This script must be run directly from the dotfiles repository:
-# cd path/to/dotfiles
+# git clone --recursive https://github.com/nsgrantham/dotfiles
+# cd dotfiles
 # ./install.sh
 
-# Create symbolic links of dotfiles to home directory
+# Make directory to copy and store the existing dotfiles
 
 old_dotfiles=$HOME/.old_dotfiles/$(date '+%Y-%m-%dT%H:%M:%S')
 mkdir -p $old_dotfiles
 echo "Existing dotfiles will be moved to $old_dotfiles"
 
+# Create symbolic links of dotfiles to the home directory
+
 symlink_to_home_dir () {
   if [ -f "$HOME/$1" ]; then
-    cp -R $HOME/$1 $old_dotfiles
+    cp -RL $HOME/$1 $old_dotfiles
   fi
-  echo -n "Symlinking $1 ... "
+  echo -n "Symlinking $1 to home directory ... "
   ln -sfn "$PWD/$1" "$HOME/$1"
   echo "Done."
 }
@@ -28,6 +30,22 @@ symlink_to_home_dir .tmux.conf
 symlink_to_home_dir .config
 symlink_to_home_dir .zshrc
 symlink_to_home_dir zsh
+
+# Create symbolic links of VS Code dotfiles to the app's user settings directory
+
+vscode_user_settings="$HOME/Library/Application Support/Code/User"
+
+cp -RL "$vscode_user_settings/settings.json" $old_dotfiles
+cp -RL "$vscode_user_settings/keybindings.json" $old_dotfiles
+cp -RL "$vscode_user_settings/snippets" $old_dotfiles
+
+echo -n "Symlinking dotfiles for VS Code to its user settings directory ... "
+
+ln -sfn "$PWD/.vscode/settings.json" "$vscode_user_settings/settings.json"
+ln -sfn "$PWD/.vscode/keybindings.json" "$vscode_user_settings/keybindings.json"
+ln -sfn "$PWD/.vscode/snippets" "$vscode_user_settings/snippets"
+  
+echo "Done."
 
 # Perform macOS-specific installation
 
